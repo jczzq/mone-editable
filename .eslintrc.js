6d15968178d17c6aee62961c4033198863721f5c
jczzq <1195262383@qq.com>
2017-08-16 10:26:55 +0800

    [fix]-eslint-规则调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,11 @@ module.exports = {
     'arrow-parens': 0,    // 箭头函数用小括号括起来
     'generator-star-spacing': 0,	// 生成器函数*的前后空格
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, 	// 禁止使用debugger
-	// 函数符号前不要空格
-	"space-before-function-paren": [0, "always"]
+    // 函数符号前不要空格
+    "space-before-function-paren": [0, "always"],
+    // 语句强制分号结尾
+    "semi": [2, "always"],
+    // 必须使用全等
+    "eqeqeq": "off"
   }
 }
